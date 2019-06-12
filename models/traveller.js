d6f1011973425b69b1f8908fa199f7e7629d8941
shaunhk <shaunhk@hotmail.com>
2019-06-12 23:03:18 +0100

    done

---
+++ models/traveller.js
@@ -0,0 +1,31 @@
+const Traveller = function(journeys) {
+  this.journeys = journeys;
+};
+
+Traveller.prototype.getJourneyStartLocations = function() {
+  return this.journeys.map((journey) => {return journey.startLocation})
+};
+
+Traveller.prototype.getJourneyEndLocations = function () {
+  return this.journeys.map((journey) => {return journey.endLocation})
+};
+
+Traveller.prototype.getJourneysByTransport = function (transport) {
+  return this.journeys.filter((journey) => {return journey.transport === transport})
+};
+
+Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
+  return this.journeys.filter((journey) =>
+  {return journey.distance > minDistance})
+};
+
+Traveller.prototype.calculateTotalDistanceTravelled = function () {
+  return this.journeys.reduce((runningTotal, journey) =>
+  {return runningTotal + journey.distance}, 0)
+};
+
+Traveller.prototype.getUniqueModesOfTransport = function () {
+  return Array.from(new Set(this.journeys.map((journey) => {return journey.transport})))
+};
+
+module.exports = Traveller;
